a7ce264a87430c49576d433a770aab85da71e10f
David Rogers <rogersdw@gmail.com>
2022-07-27 15:23:33 -0700

    Finished with module 5

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,12 +1,20 @@
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+//builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
-
+builder.Services.AddDbContext<
+    BethanysPieShopDbContext>(options =>
+    {
+        options.UseSqlServer(builder.
+            Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+    });
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
@@ -18,5 +26,5 @@ if (app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.MapDefaultControllerRoute();
-
+DbInitializer.Seed(app);
 app.Run();
